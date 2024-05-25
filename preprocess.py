37a2cd8179a75e39095011a5075d1f5789c30b6f
Sascha Petznick <sambesi@hotmail.de>
2024-05-25 19:58:17 +0200

    Add function for val set and csv for joint scoring

--- preprocess.py
+++ preprocess.py
@@ -23,7 +23,15 @@ def normalize_data(df):
     return df_normalized
 
 def create_datasplit(df):
-    # Split the data into training and test sets
+    """Create a train, test, and validation split of the data.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing the data
+    Returns:
+        pd.DataFrame: DataFrame containing the training data
+        pd.DataFrame: DataFrame containing the test data
+        pd.DataFrame: DataFrame containing the validation data
+    """
     random_state = 33
     train_df, test_df = sklearn.model_selection.train_test_split(df, test_size=0.2, random_state=random_state)
     test_df, val_df = sklearn.model_selection.train_test_split(test_df, test_size=0.5, random_state=random_state)
@@ -53,8 +61,50 @@ def compute_simultaneity(user1_timeries: np.ndarray, user2_timeries: np.ndarray)
     return simultaneity_score
 
 
-def compute_and_store_simultaneity_scores(df: pd.DataFrame):
-    pass
+def compute_and_store_simultaneity_scores(df_consumers: pd.DataFrame, df_producers: pd.DataFrame, filename=None) -> pd.DataFrame:
+    """Compute the simultaneity scores between consumers and producers based on their time series data.
+
+    The consumer to producer score is computed by
+        1- sum(max(consumer_timeseries - producer_timeseries, 0)) / sum(consumer_timeseries).
+    Flipping the roles yields the second score.
+    The joint score is computed based on the two scores by
+        score_1 + scores_2 - abs(score_1 - score_2).
+
+    If a filename is provided, the output DataFrame is stored as a CSV file.
+
+    The output DataFrame contains the
+        consumerid, producerid, scoreC2P, scoreP2C, jointScore
+
+    Args:
+        df_consumers (pd.DataFrame): DataFrame containing the consumers' timeries
+        df_producers (pd.DataFrame): DataFrame containing the consumers' timeries
+    Returns:
+        pd.DataFrame: DataFrame containing the simultaneity scores where each row
+        contains the consumerid, producerid, score consumer to producer, score producer to consumer, joint score
+    """
+    # Initialize the DataFrame to store the simultaneity scores
+    entries = []
+
+    # Iterate over the consumers and producers to compute the simultaneity scores
+    for consumer_id, consumer in df_consumers.iterrows():
+        for producer_id, producer in df_producers.iterrows():
+            if producer_id == consumer_id:
+                continue
+            # Compute the simultaneity scores
+            score_c2p = compute_simultaneity(consumer, producer)
+            score_p2c = compute_simultaneity(producer, consumer)
+            joint_score = score_c2p + score_p2c - abs(score_c2p - score_p2c)
+
+            # Store the scores in the DataFrame
+            entries.append({'consumerid': consumer_id, 'producerid': producer_id,
+                                                      'scoreC2P': score_c2p, 'scoreP2C': score_p2c, 'jointScore': joint_score})
+
+    df_simultaneity = pd.DataFrame(entries)
+    if filename:
+        df_simultaneity.to_csv(filename, index=False)
+    return df_simultaneity
+
+
 
 
 if __name__ == '__main__':
@@ -64,21 +114,28 @@ if __name__ == '__main__':
 
     # Read the CSV data
     # filename = StringIO(data)
-    filename = 'dataset/consumption-1y.csv'
-    df = pd.read_csv(filename, sep=';', usecols=["Id", "Volumes"])
+    # filename = 'dataset/production-1y.csv'
+    # df = pd.read_csv(filename, sep=';', usecols=["Id", "Volumes"])
 
-    # Apply the function to the Volumes column
-    df['Volumes'] = df['Volumes'].apply(parse_volumes)
+    # # Apply the function to the Volumes column
+    # df['Volumes'] = df['Volumes'].apply(parse_volumes)
 
-    # Create a DataFrame from the 'Volumes' dictionaries
-    volumes_df = df['Volumes'].apply(pd.Series)
+    # # Create a DataFrame from the 'Volumes' dictionaries
+    # volumes_df = df['Volumes'].apply(pd.Series)
 
-    # Concatenate the original DataFrame with the new DataFrame of volumes
-    result_df = pd.concat([df.drop(columns=['Volumes']), volumes_df], axis=1)
-    result_df.to_csv('dataset/consumption-1y-processed.csv', sep=';', index=False)
+    # # Concatenate the original DataFrame with the new DataFrame of volumes
+    # producer_df = pd.concat([df.drop(columns=['Volumes']), volumes_df], axis=1)
+    # producer_df.to_csv('dataset/production-1y-processed.csv', sep=';', index=False)
 
     # # Print the final DataFrame
-    # print(result_df.info())
-    # print(result_df.head())
+    # print(producer_df.info())
+    # print(producer_df.head())
 
-    # _, _, _ = create_datasplit(result_df)
+    consumer_df = pd.read_csv('dataset/consumption-1y-processed.csv', sep=';')
+    producer_df = pd.read_csv('dataset/production-1y-processed.csv', sep=';')
+
+    c_test_set, c_train_set, c_val_set = create_datasplit(consumer_df)
+    p_test_set, p_train_set, p_val_set = create_datasplit(producer_df)
+    _ = compute_and_store_simultaneity_scores(c_val_set, p_val_set, 'dataset/val_test_joint_scoring.csv')
+
+
